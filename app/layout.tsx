0cfc690a12252a305cc969a671590ea73bb42692
Jay Doan <jay@doan.works>
2024-08-11 18:00:33 -0500

    Update progress wheel on resize and navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,8 +57,8 @@ export default function RootLayout({
       <head>
         <Script src="../progress-wheel.js" strategy="afterInteractive" />
       </head>
-      <body className="antialiased mx-4 mt-8 lg:mx-auto">
-        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+      <body className="antialiased mx-4 lg:mx-auto">
+        <main className="flex-auto min-w-0 mt-4 flex flex-col px-2 md:px-0">
           <ProgressWheel />
           <Navbar />
           {children}
--- public/progress-wheel.js
+++ public/progress-wheel.js
@@ -1,19 +1,32 @@
-const updateProgress = () => {
+function updateProgress() {
   const scrollTop = window.scrollY;
   const docHeight = document.body.scrollHeight;
   const winHeight = window.innerHeight;
   const trueScrollPercent = scrollTop / (docHeight - winHeight);
-  const scrollPercent = Math.floor(
-    Math.max(Math.min(trueScrollPercent * 100, 100), 0)
-  );
+  const scrollPercent =
+    docHeight <= winHeight
+      ? 100
+      : Math.floor(Math.max(Math.min(trueScrollPercent * 100, 100), 0));
 
   const progressElement = document.querySelector(".circular-progress");
   progressElement.style.setProperty("--progress", scrollPercent);
 
   const progressText = document.querySelector(".progress-text");
   progressText.innerHTML = `${scrollPercent}%`;
-};
+}
 
 updateProgress();
 
 document.addEventListener("scroll", updateProgress);
+window.addEventListener("resize", updateProgress);
+
+let previousUrl = "";
+const observer = new MutationObserver(function (mutations) {
+  if (location.href !== previousUrl) {
+    previousUrl = location.href;
+    console.log(`URL changed to ${location.href}`);
+    updateProgress();
+  }
+});
+const config = { subtree: true, childList: true };
+observer.observe(document, config);
