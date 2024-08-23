b499df835934b5b93329fd6e1221adfcb96d3f8d
Jay Doan <jay@doan.works>
2024-08-23 06:52:40 -0500

    Native app copy

--- app/components/why.tsx
+++ app/components/why.tsx
@@ -15,33 +15,38 @@ export default function Why() {
           <div className="c-right column">
             <h2 className="h2">Reach your audience on any platform</h2>
             <br />
-            <p>
+            <p style={{ textAlign: "justify" }}>
               We specialize in fully responsive hybrid applications, which means
               a single codebase can deliver a website, iOS app, Android app, and
-              more. This approach not only guarantees a beautiful, seamless
+              more. This approach not only guarantees a seamless and beautiful
               experience on any screen size but also accelerates your time to
               market. Whether you're launching a new product or expanding an
               existing one, hybrid applications offer the best way to maximize
-              reach and engage your audience across all pltforms.
+              reach and engage your audience across all platforms.
             </p>
           </div>
         </div>
+        <br />
         <div className="benefit">
           <div className="c-left column">
-            <h2 className="h2">hello</h2>
-            <p>hey there</p>
+            <h2 className="h2">Deliver Unmatched Performance</h2>
+            <br />
+            <p style={{ textAlign: "justify" }}>
+              Looking for a more powerful user experience? Native apps offer
+              unparalleled performance and deep integration with
+              platform-specific features, providing a seamless user experience
+              across iOS, Android, macOS, and Windows. By leveraging the full
+              capabilities of each operating system, native apps deliver faster
+              load times, smoother interactions, and access to advanced
+              functionalities that cross-platform solutions haven't yet caught
+              up to. Whether you're aiming to create a robust mobile app or a
+              powerful desktop application, building natively ensures that your
+              product is optimized for the platform, offering your users the
+              highest level of performance and reliability.
+            </p>
           </div>
           <div className="c-right" style={{ justifyContent: "center" }}>
             <Swatch />
-          </div>
-        </div>
-        <div className="benefit">
-          <div className="c-left" style={{ justifyContent: "center" }}>
-            <Swatch />
-          </div>
-          <div className="c-right column">
-            <h2 className="h2">hello</h2>
-            <p>hey there</p>
           </div>
         </div>
       </div>
