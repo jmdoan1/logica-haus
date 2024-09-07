1193f06bfc0661ec85130d86bd0584df91b77feb
Jay Doan <jay@doan.works>
2024-09-07 17:10:33 -0500

    + contact-thanks page

---
+++ app/contact-thanks/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <section>
+      <div style={{ textAlign: "center" }}>
+        <h1 className="h1">Thanks for reaching out!</h1>
+        <h3 className="h3">We will get in touch with you shortly :)</h3>
+      </div>
+    </section>
+  );
+}
